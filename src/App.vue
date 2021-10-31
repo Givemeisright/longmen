16d5d9204d166418188f4e1b8ab4737ad768b41a
lxg <479161553@qq.com>
2021-10-31 10:13:10 +0800

    updata money.vue

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,27 @@
 <template>
-  <div id="app nav">
+  <div id="app">
     <router-view />
-    
   </div>
 </template>
 
 <style lang="scss">
- * {
-    margin: 0; padding: 0;
-    box-sizing: border-box;
-  }
-  body{
-    line-height: 1.5;
-  
-  }
-  a{
-    text-decoration: none;
-    color: inherit;
-  }
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2C3E50;
-  }
-  #nav {
-    padding: 30px;
-    a {
-      font-weight: bold;
-      color: #2C3E50;
-      &.router-link-exact-active {
-        color: #42B983;
-      }
-    }
-  }
+@import "~@/assets/style/helper.scss";
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  line-height: 1.5;
+  font-family: $font-hei;
+  color: #333;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
 </style>
